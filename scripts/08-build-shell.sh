d51f2c811c2a4d64f181572fcb2d3665bbf1a7a6
ladybug-me <rajveer2027singh@gmail.com>
2026-07-21 09:43:27 +0530

    fix: remove --needed flag from dnf install command in build script

--- scripts/08-build-shell.sh
+++ scripts/08-build-shell.sh
@@ -42,7 +42,7 @@ if [[ "${CAELESTIA_SETUP_RUNNING:-0}" == "0" ]]; then
         sudo pacman -S --needed qt6-wayland --noconfirm || warn "qt6-wayland install failed..."
     elif command -v dnf >/dev/null; then
         info "Installing via dnf..."
-        sudo dnf install --needed qt6-qtwayland qt6-qtwayland-devel kf6-kglobalaccel-devel qt6-qtbase-private-devel -y || warn "qt6-qtwayland qt6-qtwayland-devel kf6-kglobalaccel-devel qt6-qtbase-private-devel install failed..."
+        sudo dnf install qt6-qtwayland qt6-qtwayland-devel kf6-kglobalaccel-devel qt6-qtbase-private-devel -y || warn "qt6-qtwayland qt6-qtwayland-devel kf6-kglobalaccel-devel qt6-qtbase-private-devel install failed..."
     fi
     
     if [[ "${CAELESTIA_SKIP_DEPLOY:-0}" == "0" ]]; then
